46bcb6e298bc4e206f460a06fda5588a35f6330c
Giftxipu <gift.xipu@gmail.com>
2025-01-13 20:56:33 +0200

    fixed dependencie

--- models/User.go
+++ models/User.go
@@ -1,8 +1,11 @@
 package models
 
+// User represents the structure for user input data
 type User struct {
-  Weight float64 `json:"weight"`;
-  Height float64 `json:"height"`;
-  Age int `json:"age"`;
-  Gender string `json:"gender"`;
+	Weight        float64 `json:"weight" binding:"required"`
+	Height        float64 `json:"height" binding:"required"`
+	Age           int     `json:"age" binding:"required"`
+	Gender        string  `json:"gender" binding:"required"`
+	ActivityLevel string  `json:"activity_level" binding:"required"` // Add this field
 }
+
